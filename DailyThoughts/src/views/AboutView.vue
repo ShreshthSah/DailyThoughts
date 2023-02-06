2511fcac92e69b4868261c0dacc32e37c17266bd
SHRESHTH YT <shreshthrajvijaykumarsah@outlook.com>
2023-02-06 18:03:22 +0530

    updated

--- DailyThoughts/src/views/AboutView.vue
+++ DailyThoughts/src/views/AboutView.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="head">
+    <h1>About</h1>
+    <p class="mx-2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tempora
+    libero possimus enim magni accusamus ut dolores, nesciunt quaerat culpa. Est
+    odit doloribus praesentium, cupiditate impedit consequatur optio? Aperiam
+    maxime facere alias eveniet corporis, recusandae reiciendis, nulla maiores
+    cupiditate explicabo similique nam? Est nostrum animi, quia aperiam tempora
+    sit impedit similique incidunt? Fugiat exercitationem assumenda tenetur
+    illum voluptates, cumque quidem minima adipisci enim id beatae itaque iusto,
+    sint at accusamus quod. Sapiente dolor architecto ab alias totam sed
+    exercitationem illo accusantium, qui nulla a recusandae? Omnis eius esse
+    ipsam optio voluptates corporis, fuga accusantium suscipit quasi? Alias
+    saepe, vitae dolor tempore, sunt quasi corrupti eveniet tempora explicabo
+    excepturi in hic voluptates, ad vero nesciunt. Magni tempore ut in aut
+    repudiandae est possimus facilis cumque itaque eum? Blanditiis tenetur,
+    iusto excepturi reprehenderit obcaecati officiis aliquam explicabo
+    architecto. Obcaecati accusamus voluptatibus rerum et nostrum,
+    necessitatibus inventore in, omnis, ratione repudiandae nulla pariatur esse
+    quisquam nobis quia fuga odio ducimus! Repudiandae sapiente, nam velit
+    reprehenderit quod quo quis, quae aperiam consequuntur tempora ex veritatis
+    harum molestias nulla non minus distinctio atque eos iusto, quia sunt optio
+    explicabo. Dolorum architecto corporis voluptatibus voluptatem maxime
+    quaerat fugit ex alias debitis, quos atque facilis odit fuga!
+    </p>
   </div>
 </template>
 
--- DailyThoughts/src/views/HomeView.vue
+++ DailyThoughts/src/views/HomeView.vue
@@ -3,25 +3,59 @@
 </script>
 
 <template>
-  <!-- <h1>DailyThought</h1> -->
-  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpapercave.com%2Fgood-thoughts-wallpapers&psig=AOvVaw2M2YW6ZcKyx5kPDi46ivbs&ust=1675771479832000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCODHwq_tgP0CFQAAAAAdAAAAABAE" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpapercave.com%2Fgood-thoughts-wallpapers&psig=AOvVaw2M2YW6ZcKyx5kPDi46ivbs&ust=1675771479832000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCODHwq_tgP0CFQAAAAAdAAAAABAE" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpapercave.com%2Fgood-thoughts-wallpapers&psig=AOvVaw2M2YW6ZcKyx5kPDi46ivbs&ust=1675771479832000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCODHwq_tgP0CFQAAAAAdAAAAABAE" class="d-block w-100" alt="...">
-    </div>
+    <div class="card">
+      <!-- <h5 class="card-header">Featured</h5> -->
+      <div class="card-body">
+        <!-- <h5 class="card-title">Special title treatment</h5> -->
+        <p class="card-text">Read your daily Thoughts...</p>
+        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+        
+        <RouterLink class="btn btn-primary" to="/thoughts">Click here</RouterLink>
   </div>
 </div>
-<!-- <div id="image">  <img src='https://images.unsplash.com/photo-1674596346743-89398a9dde7b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NTU0ODM1NA&ixlib=rb-4.0.3&q=80&w=1080' alt=""/></div> -->
-    <div id="item">
-      <h3>Read your daily thought</h3>
-      <button><RouterLink to="/thoughts">Click here</RouterLink></button>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img  src="https://wallpaperaccess.com/full/1915195.jpg" class="d-block w-70" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG5aFBX7uDGfEptcC-HvN7U3CHMd4iuvtmYQ&usqp=CAU" class="d-block w-70" alt="">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="hhttps://wallpaperaccess.com/full/1915153.jpg" class="d-block w-70" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- <div id="image">  <img src='https://images.unsplash.com/photo-1674596346743-89398a9dde7b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NTU0ODM1NA&ixlib=rb-4.0.3&q=80&w=1080' alt=""/></div> -->
+    <!-- <div id="item">
+      <h3>Read your daily thought</h3>
+    </div> -->
+  
  
 </template> 
 
@@ -67,11 +101,7 @@ export default{
           // console.log(this.users)
 </script>
 <style>
-#image{
-  position:relative;
-  left:0px;
-  top:0px;
-}
+
 h3{
   color: blue;
   font-size: 30px;
@@ -86,4 +116,5 @@ button{
   top: 100px;
   left:100px;
 }
+
 </style>
