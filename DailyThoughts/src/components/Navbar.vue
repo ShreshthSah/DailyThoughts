5d071e289975e0cf8666c19614e4427b2487a70f
SHRESHTH YT <shreshthrajvijaykumarsah@outlook.com>
2023-02-06 17:23:52 +0530

    updated

--- DailyThoughts/src/components/Navbar.vue
+++ DailyThoughts/src/components/Navbar.vue
@@ -6,23 +6,24 @@
    <!-- <div><router-link to="/register">Login</router-link></div> -->
    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#">DailyThoughts</a>
+    <!-- <a class="navbar-brand" href="#">DailyThoughts</a> -->
+      <div ><router-link class="navbar-brand fs-italic" to="/">DailyThoughts</router-link></div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item mx-2">
-          <router-link to="/thoughts">Home</router-link>
+          <router-link class="nav-link fs-16px" to="/thoughts">Thoughts</router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/about">About</router-link>
+          <router-link class="nav-link fs-16px" to="/about">About</router-link>
         </li>
         
       
       </ul>
-          <div className="nav-tem mx-2"> 
-            <router-link to="/login">
+          <div class="nav-item mx-2"> 
+            <router-link class="nav-link fs-15px" to="/login">
               Login
             </router-link>
            </div>
